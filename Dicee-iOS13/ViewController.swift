78cbe7a552903dc18463433adeae735d89059f2d
Emmanuel George <Emmanuelgeorge1000@gmail.com>
2022-09-27 22:17:01 +0530

    new

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -9,12 +9,31 @@
 import UIKit
 
 class ViewController: UIViewController {
+//IBOutlet allows me to refernce a UI element
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+        //who           what   value
+//        diceImageView1.image = #imageLiteral(resourceName: "DiceSix")
+       // diceImageView1.alpha = 0.5;
+//        diceImageView2.image = #imageLiteral(resourceName: "DiceTwo")
+        
+//    }
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        let diceArray = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
+//      print("Buton got tapped.")
+        diceImageView1.image = diceArray.randomElement()
+       //randomElement will select a random number from the array
+        diceImageView2.image = diceArray[Int.random(in: 0...5)]
+        //Int.random(in:0...n) will basically do the same but we can specify the start and end of it
+        
+  
+        
+    
 }
 
+}
